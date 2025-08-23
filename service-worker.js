500c55bd93205d286fe947cd3202bb6d1c2c9eb5
saeyoung-2025 <saeyoung2016@gmail.com>
2025-08-23 13:14:37 +0900

    cl;aude사용 자기 고유 툴언어로 하는 듯 기본 형식과 다름 주의 파일명들 참조

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "checklist-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/service-worker.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
